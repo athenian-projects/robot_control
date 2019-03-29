cf45471d4924a40277c398f2e548c63801b12f2f
Tarun <tarunchichili@gmail.com>
2019-03-29 10:14:30 -0700

    add spelling correction

--- lib/speach.dart
+++ lib/speach.dart
@@ -5,7 +5,7 @@ class Speach extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Speach Control"),
+        title: Text("Speech Control"),
       ),
       body: Center(),
     );
