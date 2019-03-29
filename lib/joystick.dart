da67c717c946370a52765f547fbaa2baaf282c59
omanavekar <om.anavekar@gmail.com>
2019-03-29 11:19:10 -0700

    Add Joystick (non functional)

--- lib/joystick.dart
+++ lib/joystick.dart
@@ -33,6 +33,7 @@ class _TestForm extends State<TestForm> {
               new Expanded(
                   flex: 2,
                   child: touchControl()
+
               ),
               new Expanded(
                 flex: 1,
