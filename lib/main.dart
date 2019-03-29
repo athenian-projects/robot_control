39d9fb363ca9c5e035d47062cab0410b2f24e6d1
Matthew Winchell <winchell587matthew@gmail.com>
2019-03-29 11:00:02 -0700

    add SplashScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 
@@ -9,15 +10,106 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Robot Control',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Robot Control'),
+        title: 'Robot Control',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: SplashScreen()
+
     );
   }
 }
 
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(
+        Duration(seconds: 5),
+            () =>
+            Navigator.of(context).pushReplacement(MaterialPageRoute(
+                builder: (BuildContext context) =>
+                    MyHomePage(title: 'Button Control'))));
+  }
+
+  @override
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(
+          fit: StackFit.expand,
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(color: Colors.blueAccent),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Expanded(
+                    flex: 2,
+                    child: Container(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            CircleAvatar(
+                              backgroundColor: Colors.deepOrange,
+                              radius: 50.0,
+                              child: Icon(
+                                const IconData(
+                                    0xe900, fontFamily: 'AthenianOwl'),
+                                color: Colors.white,
+                                size: 50.0,
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(top: 10.0),
+                            ),
+                            Text("Best App Ever. Change my mind",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 24.0),
+                            )
+                          ],
+                        )
+                    )
+                ),
+                Expanded(
+                    flex: 1,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        CircularProgressIndicator(
+                          valueColor: AlwaysStoppedAnimation(Colors.white),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(top: 20.0),
+                        ),
+                        Text(
+                            "Loading...",
+                            softWrap: true,
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18.0)),
+                      ],
+                    )
+                )
+              ],
+            )
+          ],
+        )
+    );
+  }
+}
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
   final String title;
@@ -215,3 +307,5 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
+
