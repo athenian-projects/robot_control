8dd3f17cb4f7a22633e4cf8604cb2d4f43273011
omanavekar <om.anavekar@gmail.com>
2019-03-29 10:21:45 -0700

    Merge branch 'master' of /Users/omanavekar/Development/IntelliJ_IDEA/robot_control with conflicts.

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,6 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 
-import 'imu.dart';
-import 'joystick.dart';
-import 'speach.dart';
-import 'videoStream.dart';
-
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -18,7 +13,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Button Control'),
+      home: MyHomePage(title: 'Robot Control'),
     );
   }
 }
@@ -32,97 +27,49 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-  //Navigation Bar
-  int _selectedIndex = 0;
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-    if (_selectedIndex == 1) {
-      Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => Joystick()),
-      );
-    }
-    if (_selectedIndex == 2) {
-      Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => IMU()),
-      );
-    }
-    if (_selectedIndex == 4) {
-      Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => VideoStream()),
-      );
-    }
-    if (_selectedIndex == 3) {
-      Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => Speach()),
-      );
-    }
-
-  }
-
-  final _widgetOptions = [
-    Text('Index 0: Control'),
-    Text('Index 1: Video Feed'),
-    Text('Index 2: Settings'),
-  ];
-
   final String _prefix = "http://10.16.104.100:8080/";
-
-  //Posotranics
-  //final String _prefix = "http://192.168.1.182:8080/";
 
   //final String _prefix = "http://ros.local:8080/";
 
   var _linear = 0.0;
   var _angular = 0.0;
-  var _lastLinear = 0.0;
-  var _lastAngular = 0.0;
 
-  void _makeAbsoluteRequest(String type, double val) async {
-    var uri = _prefix + type + "?val=" + val.toStringAsPrecision(1);
-    //print(uri);
-    var request = await HttpClient().getUrl(Uri.parse(uri));
+  void _makeRestRequest(String direction) async {
+    var request = await HttpClient().getUrl(Uri.parse(_prefix + direction));
     var response = await request.close();
     await for (var contents in response.transform(Utf8Decoder())) {
-      try {
+      //print(contents);
+      if (contents
+          .trim()
+          .length > 0) {
         var json = jsonDecode(contents);
-        print(json);
+        //print(json);
         setState(() {
           _linear = json['linear'];
           _angular = json['angular'];
         });
-      }
-      on FormatException {
-        // Ignore
       }
     }
   }
 
-  void _makeRelativeRequest(String direction) async {
-    var uri = Uri.parse(_prefix + direction);
-    //print(uri);
-    var request = await HttpClient().getUrl(uri);
-    var response = await request.close();
-    await for (var contents in response.transform(Utf8Decoder())) {
-      try {
-        var json = jsonDecode(contents);
-        print(json);
-        setState(() {
-          _linear = json['linear'];
-          _angular = json['angular'];
-        });
-      }
-      on FormatException {
-        // Ignore
-      }
-    }
+  void _forward() {
+    _makeRestRequest('forward');
+  }
+
+  void _backward() {
+    _makeRestRequest('backward');
+  }
+
+  void _left() {
+    _makeRestRequest('left');
+  }
+
+  void _right() {
+    _makeRestRequest('right');
+  }
+
+  void _stop() {
+    _makeRestRequest('stop');
   }
 
   @override
@@ -141,58 +88,42 @@ class _MyHomePageState extends State<MyHomePage> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
                   Text(
-                    'Linear: $_linear  Angular: ${_angular != 0.0 ? -1 * _angular : _angular}',
+                    'Linear: ',
+                    style: Theme
+                        .of(context)
+                        .textTheme
+                        .display1,
+                  ),
+                  Text(
+                    '$_linear',
+                    style: Theme
+                        .of(context)
+                        .textTheme
+                        .display1,
+                  ),
+                  Text(
+                    '  Angular: ',
+                    style: Theme
+                        .of(context)
+                        .textTheme
+                        .display1,
+                  ),
+                  Text(
+                    '$_angular',
                     style: Theme
                         .of(context)
                         .textTheme
                         .display1,
                   ),
                 ],
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.only(bottom: 40.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Slider(
-                    activeColor: Colors.indigoAccent,
-                    label: "Linear",
-                    min: -1,
-                    max: 1,
-                    divisions: 20,
-                    onChangeStart: (newVal) => _lastLinear = _linear,
-                    onChanged: (newVal) {
-                      if (newVal != _lastLinear) {
-                        _makeAbsoluteRequest("linear", newVal);
-                        _lastLinear = newVal;
-                      }
-                    },
-                    value: _linear,
-                  ),
-                  Slider(
-                    activeColor: Colors.indigoAccent,
-                    label: "Angular",
-                    min: -1,
-                    max: 1,
-                    divisions: 20,
-                    onChangeStart: (newVal) => _lastAngular = _angular,
-                    onChanged: (newVal) {
-                      if (newVal != _lastAngular) {
-                        _makeAbsoluteRequest("angular", newVal != 0.0 ? -1 * newVal : newVal);
-                        _lastAngular = newVal;
-                      }
-                    },
-                    value: -1 * _angular,
-                  ),
-                ],
+
               ),
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 RaisedButton(
-                  onPressed: () => _makeRelativeRequest('forward'),
+                  onPressed: _forward,
                   child: const Text('Forward'),
                 ),
               ],
@@ -203,18 +134,18 @@ class _MyHomePageState extends State<MyHomePage> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
                   RaisedButton(
-                    onPressed: () => _makeRelativeRequest('left'),
+                    onPressed: _left,
                     child: const Text('Left'),
                   ),
                   Container(
                     margin: const EdgeInsets.only(left: 20.0, right: 20.0),
                     child: RaisedButton(
-                      onPressed: () => _makeRelativeRequest('stop'),
+                      onPressed: _stop,
                       child: const Text('Stop'),
                     ),
                   ),
                   RaisedButton(
-                    onPressed: () => _makeRelativeRequest('right'),
+                    onPressed: _right,
                     child: const Text('Right'),
                   ),
                 ],
@@ -224,7 +155,7 @@ class _MyHomePageState extends State<MyHomePage> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 RaisedButton(
-                  onPressed: () => _makeRelativeRequest('backward'),
+                  onPressed: _backward,
                   child: const Text('Backward'),
                 ),
               ],
@@ -232,25 +163,6 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-              icon: Icon(Icons.home), title: Text('Control')),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.videogame_asset), title: Text('Joystick')),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.launch), title: Text('IMU')),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.phone), title: Text('Speach')),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.voice_chat), title: Text('Video')),
-        ],
-        currentIndex: _selectedIndex,
-        fixedColor: Colors.deepOrange,
-        onTap: _onItemTapped,
-      ),
     );
   }
 }
-
